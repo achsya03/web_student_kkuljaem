ba5563bed033f8ce376f22e726f3506aa4bbe96c
achmad syarif <ach.sya03@gmail.com>
2022-05-11 19:46:01 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        URL::forceScheme('http');
     }
 
     /**
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        URL::forceScheme('http');
     }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,8 +35,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->configureRateLimiting();
-        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
 
 
         parent::boot();
